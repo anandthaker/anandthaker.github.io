eb5f9930adef3f7cd2f9ad204aa23128ebb6e12a
anandthaker <41373979+anandthaker@users.noreply.github.com>
2024-03-07 10:17:51 -0500

    Update feed-direct.js

--- feed-direct.js
+++ feed-direct.js
@@ -18,12 +18,12 @@
             var months = Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
             var strDate = months[date.getMonth()] + ", "+ date.getDate() + " " + date.getFullYear()
             
-              var card = '<div style="font-family: sans-serif; margin-bottom: 15px;">' +
+              var card = '<div style="font-family: font-family: ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji; margin-bottom:15px; margin-bottom: 15px;">' +
                               '<a style="text-decoration: none; color: black;" href="' + item.link + '" target="_blank">' +
                                   '<div style="font-weight: 700; font-size: 1.125rem; line-height: 1.75rem;">' + item.title + '</div>' + 
-                                  '<img style="float: left; margin: 0 15px 0 0; width: 130px; border-radius: 0.5rem; display: block; " src="' + item.enclosure.link + '" alt="Image">' + 
-                                  '<p style="color: #707070;">' + item.description + '</p>' +
-                                  '<p style="color: #707070;">' + item.description + '</p>' +
+                                  '<img style="float: left; margin: 0 15px 0 0; width: 200px; border-radius: 0.5rem; display: block; " src="' + item.enclosure.link + '" alt="Image">' + 
+                                  '<p style="font-weight: 100;color: #707070;line-height: 1.5rem;">' + item.description + '</p>' +
+                                  '<p style="font-weight: 100;color: #707070;line-height: 1.5rem;">' + strDate + '</p>' +
                               '</a>' + 
                           '</div>';
               feedContainer.append(card);
