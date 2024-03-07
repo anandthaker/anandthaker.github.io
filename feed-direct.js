9710c980d5380712e30b5bad5bc26f4d7f33e5ad
anandthaker <41373979+anandthaker@users.noreply.github.com>
2024-03-07 09:52:47 -0500

    Update feed-direct.js

--- feed-direct.js
+++ feed-direct.js
@@ -13,10 +13,11 @@
             // Parse and display feed content
             var feedContainer = $('#feed-container');
             data.items.forEach(function(item) {
-              var card = '<div class="card"><a href="' + item.link + '" target="_blank">' +
-                          '<div style="font-weight: 700; font-size: 1.125rem; line-height: 1.75rem; font-family:ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;">' + item.title + '</div>' + 
-                          '<img style="width: 130px; border-radius: 0.25rem; display: block;" size src="' + item.enclosure.link + '" alt="Image">' + 
-                          '<div>' + item.description + '</div>' +
+              var card = '<div style="font-family: sans-serif;">' +
+                          '<a style="text-decoration: none; color: black;" href="' + item.link + '" target="_blank">' +
+                          '<div style="font-weight: 700; font-size: 1.125rem; line-height: 1.75rem;">' + item.title + '</div>' + 
+                          '<img style="float: left; margin: 0 15px 0 0; width: 130px; border-radius: 0.5rem; display: block; " src="' + item.enclosure.link + '" alt="Image">' + 
+                          '<p style="color: #707070;">' + item.description + '</p>' +
                           '</a></div>';
               feedContainer.append(card);
             });
